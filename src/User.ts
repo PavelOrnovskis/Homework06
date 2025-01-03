63667de556d06417a8fd6c536f1e8cc326decd08
PavelOrnovskis <pavel.ornovskis@gmail.com>
2025-01-03 17:35:21 +0200

    Added new methods and tests

---
+++ src/User.ts
@@ -0,0 +1,18 @@
+export class User {
+    name: string
+    age: number
+    surname: string
+    phoneNumber: string
+    address: string
+    consentGiven: boolean | undefined
+
+    constructor(name: string, age: number, surname: string, phoneNumber: string, address: string) {
+        this.name = name
+        this.age = age
+        this.surname = surname
+        this.phoneNumber = phoneNumber
+        this.address = address
+    }
+
+
+}
